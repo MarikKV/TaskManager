5fb89bce80ad13970c0e86dbb9bf65c97919a2c9
Marik1 <kosaniakm@gmail.com>
2019-11-20 14:32:10 +0200

    Updates

---
+++ precache-manifest.26e5a21f55a2b8ae2b50faa292b58aaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdb387feb6582e3f6c60",
+    "url": "/TaskManager/static/css/main.1ab7fb27.chunk.css"
+  },
+  {
+    "revision": "bdb387feb6582e3f6c60",
+    "url": "/TaskManager/static/js/main.e36bc55f.chunk.js"
+  },
+  {
+    "revision": "f225c75f7616ab5bd37f",
+    "url": "/TaskManager/static/js/runtime~main.ae49988c.js"
+  },
+  {
+    "revision": "0bd0fd18b9768bbbf3ae",
+    "url": "/TaskManager/static/css/2.5e80a4c0.chunk.css"
+  },
+  {
+    "revision": "0bd0fd18b9768bbbf3ae",
+    "url": "/TaskManager/static/js/2.81c7df75.chunk.js"
+  },
+  {
+    "revision": "08be063530b7171f9050414b8852b6c3",
+    "url": "/TaskManager/index.html"
+  }
+];
